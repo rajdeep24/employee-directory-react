237247e7d3282e5383edd4b2fcce91f9a37f77f9
Developer Raj <rajdeepkakar@gmail.com>
2020-10-31 15:31:59 -0400

    Updated the Navbar.jsx component to add the respective links to the pages - home, about, discover, and search.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import About from "./containers/About/About";
 import Discover from "./containers/Discover/Discover";
 import Search from "./containers/Search/Search";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
 	return (
 		<>
 			<Router>
+				<Navbar />
 				<Switch>
 					<Route exact path="/" component={About} />
 					<Route exact path="/discover" component={Discover} />
