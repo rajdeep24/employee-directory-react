e2a093bdbca851e9b518fa52a581f87d34bc254b
Developer Raj <rajdeepkakar@gmail.com>
2020-11-01 07:55:30 -0500

    Added Footer component and updated Employee Table with table, headers, body, and rows.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import EmployeeTable from "./components/EmployeeTable/EmployeeTable";
+import Footer from "./components/Footer/Footer";
 
 function App() {
 	return (
 		<>
-			<Router>
-				<Header />
-				<Switch>
-					<Route exact path="/EmployeeTable" component={EmployeeTable} />
-				</Switch>
-			</Router>
+			<Header />
+			<EmployeeTable />
+			<Footer />
 		</>
 	);
 }
